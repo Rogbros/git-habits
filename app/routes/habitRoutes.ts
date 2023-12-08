8696a690c0e74474ae2f5f50c8d797a319d1469c
Mateusz Róg <kontakt@matrog.pl>
2023-12-08 22:32:20 +0100

    Added habits page

--- app/routes/habitRoutes.ts
+++ app/routes/habitRoutes.ts
@@ -6,6 +6,10 @@ router.get('/', (req: express.Request, res: express.Response) => {
   res.render('pages/dashboard.html', {message: 'Welcome to my life'});
 });
 
+router.get('/habits', (req: express.Request, res: express.Response) => {
+  res.render('pages/habits.html');
+});
+
 router.get('/test', (req: express.Request, res: express.Response) => {
   res.render('partials/test.html');
 })
